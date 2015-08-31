5bd62bd1df5504f8445d53c7bef6f785550ccb0e
sitamjs <sitamjs@KENOW7SITAMJS.snaponglobal.com>
2015-08-31 16:51:16 -0500

    github installed

---
+++ augbatch/src/com/java/aug/conditionaloperator.java
@@ -0,0 +1,15 @@
+package com.java.aug;
+
+public class conditionaloperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age = 30;
+		boolean b;
+		b = (age>30)?true:false;
+		System.out.println("The value b is "+b);
+		
+	}
+
+}
